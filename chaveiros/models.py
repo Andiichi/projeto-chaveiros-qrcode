351af30a6686bfc560afb3a08a3271c190ba2cb2
Andrezza <andrezza.n.dasilva@gmail.com>
2025-04-16 09:00:10 -0300

    alterações nas variaveis do model do chaveiro e dos choices do auth_app

--- chaveiros/models.py
+++ chaveiros/models.py
@@ -8,23 +8,25 @@ from django.conf import settings
 
 class DadosObrigatorios(models.Model):
     criador = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='dados_obrigatorios')
-    nome = models.CharField(max_length=100, null=False, blank=False)
-    sobrenome = models.CharField(max_length=100, null=False, blank=False)
-    telefone = models.CharField(max_length=15, blank=True, verbose_name='telefone_chaveiro')
-    whatsapp = models.CharField(max_length=15, blank=True, verbose_name='whatsapp_chaveiro')
-    data_nascimento = models.DateField(null=False, blank=False)
+    nome_chaveiro = models.CharField(max_length=100, null=False, blank=False)
+    sobrenome_chaveiro = models.CharField(max_length=100, null=False, blank=False)
+    telefone_chaveiro = models.CharField(max_length=15, blank=True)
+    whatsapp_chaveiro = models.CharField(max_length=15, blank=True)
+    data_nascimento_chaveiro = models.DateField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
-    idade = models.PositiveIntegerField(blank=True, null=True, editable=False) # Esse será preenchido automaticamente
+    idade_chaveiro = models.PositiveIntegerField(blank=True, null=True, editable=False) # Esse será preenchido automaticamente
 
     def calcular_idade(self):
         hoje = date.today()
-        return hoje.year - self.data_nascimento.year - (
-            (hoje.month, hoje.day) < (self.data_nascimento.month, self.data_nascimento.day)
+        return hoje.year - self.data_nascimento_chaveiro.year - (
+            (hoje.month, hoje.day) < (self.data_nascimento_chaveiro.month, self.data_nascimento_chaveiro.day)
         )
 
     def save(self, *args, **kwargs):
-        if self.data_nascimento:
-            self.idade = self.calcular_idade()
+        if self.data_nascimento_chaveiro:
+            self.idade_chaveiro = self.calcular_idade()
         super().save(*args, **kwargs)
 
 class DadosOpcionais(models.Model):
@@ -41,36 +43,37 @@ class DadosOpcionais(models.Model):
     )
 
     CHOICES_UF = (
-        ('AC', 'Acre (AC)'),
-        ('AL', 'Alagoas (AL)'),
-        ('AP', 'Amapá (AP)'),
-        ('AM', 'Amazonas (AM)'),
-        ('BA', 'Bahia (BA)'),
-        ('CE', 'Ceará (CE)'),
-        ('DF', 'Distrito Federal (DF)'),
-        ('ES', 'Espírito Santo (ES)'),
-        ('GO', 'Goiás (GO)'),
-        ('MA', 'Maranhão (MA)'),
-        ('MT', 'Mato Grosso (MT)'),
-        ('MS', 'Mato Grosso do Sul (MS)'),
-        ('MG', 'Minas Gerais (MG)'),
-        ('PA', 'Pará (PA)'),
-        ('PB', 'Paraíba (PB)'),
-        ('PR', 'Paraná (PR)'),
-        ('PE', 'Pernambuco (PE)'),
-        ('PI', 'Piauí (PI)'),
-        ('RJ', 'Rio de Janeiro (RJ)'),
-        ('RN', 'Rio Grande do Norte (RN)'),
-        ('RS', 'Rio Grande do Sul (RS)'),
-        ('RO', 'Rondônia (RO)'),
-        ('RR', 'Roraima (RR)'),
-        ('SC', 'Santa Catarina (SC)'),
-        ('SP', 'São Paulo (SP)'),
-        ('SE', 'Sergipe (SE)'),
-        ('TO', 'Tocantins (TO)'),
+        ('AC', 'Acre '),
+        ('AL', 'Alagoas '),
+        ('AP', 'Amapá '),
+        ('AM', 'Amazonas '),
+        ('BA', 'Bahia '),
+        ('CE', 'Ceará '),
+        ('DF', 'Distrito Federal '),
+        ('ES', 'Espírito Santo '),
+        ('GO', 'Goiás '),
+        ('MA', 'Maranhão '),
+        ('MT', 'Mato Grosso '),
+        ('MS', 'Mato Grosso do Sul '),
+        ('MG', 'Minas Gerais '),
+        ('PA', 'Pará '),
+        ('PB', 'Paraíba '),
+        ('PR', 'Paraná '),
+        ('PE', 'Pernambuco'),
+        ('PI', 'Piauí '),
+        ('RJ', 'Rio de Janeiro '),
+        ('RN', 'Rio Grande do Norte '),
+        ('RS', 'Rio Grande do Sul '),
+        ('RO', 'Rondônia '),
+        ('RR', 'Roraima '),
+        ('SC', 'Santa Catarina' ),
+        ('SP', 'São Paulo '),
+        ('SE', 'Sergipe '),
+        ('TO', 'Tocantins '),
     )
 
     dados_obrigatorios = models.OneToOneField(DadosObrigatorios, on_delete=models.CASCADE, related_name='dados_opcionais')
+    foto_chaveiros = models.ImageField(upload_to='fotos_chaveiros/', blank=True, null=True) 
     endereco = models.CharField(max_length=30, blank=True)
     numero_endereco = models.CharField(max_length=10, blank=True, verbose_name='numero')
     complemento = models.CharField(max_length=30, blank=True)
@@ -82,8 +85,6 @@ class DadosOpcionais(models.Model):
     medicamentos = models.TextField(blank=True)
     link_whatsapp = models.URLField(blank=True, verbose_name='link-whats')
     links_outros = models.URLField(blank=True, verbose_name='link-outros')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.dados_obrigatorios.nome} {self.dados_obrigatorios.sobrenome}'
+        return f'{self.dados_obrigatorios.nome_chaveiro} {self.dados_obrigatorios.sobrenome_chaveiro}'
