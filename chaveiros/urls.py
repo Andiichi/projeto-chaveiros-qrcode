ade4d8c5e50437714b9784b544cee292262fd9d7
Andrezza <andrezza.n.dasilva@gmail.com>
2025-04-16 11:27:07 -0300

    arrumar o form de cadastro de chaveiros e fazer o views ainda

--- chaveiros/urls.py
+++ chaveiros/urls.py
@@ -13,12 +13,15 @@ urlpatterns = [
     path('pagina-inicial/', TemplateView.as_view(template_name='pagina-inicial.html'), name='pagina_inicial'),
 
     # Rotas do Dashboard
-    path('dashboard/', login_required(TemplateView.as_view(template_name='dashboard/dashboard_principal.html')), name='dashboard'),
-    path('dashboard/listagem', login_required(TemplateView.as_view(template_name='dashboard/dashboard_listagem.html')), name='dashboard_listagem'),
+    path('dashboard/principal/', login_required(TemplateView.as_view(template_name='dashboard/dashboard_principal.html')), name='dashboard'),
+    path('listagem-de-chaveiros/', login_required(TemplateView.as_view(template_name='dashboard/dashboard_listagem.html')), name='dashboard_listagem'),
 
     # Rotas do perfil e editar perfil
-    path('dashboard/perfil', login_required(TemplateView.as_view(template_name='dashboard/dashboard_profile.html')), name='perfil'),
-    path('dashboard/editar-perfil', atualizar_perfil, name='perfil-editar'),
+    path('perfil/', login_required(TemplateView.as_view(template_name='dashboard/dashboard_profile.html')), name='perfil'),
+    path('configurações/editar-perfil/', atualizar_perfil, name='perfil-editar'),
     path('atualizar-foto/', atualizar_foto, name='atualizar_foto'),
+
+    #Rotas de criação de chaveiros e edição
+    path('criar-novo-chaveiro/', login_required(TemplateView.as_view(template_name='dashboard/dashboard_criar_chaveiros.html')), name='criar-chaveiro'),
 
 ]
